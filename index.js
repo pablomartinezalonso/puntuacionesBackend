7095c17ae7b8244c4318c0f4032ed21e867f194b
pablomartinezalonso <pablo.mtinez.alonso@gmail.com>
2020-02-03 09:59:12 +0100

    Add funcionalidad de borrado, listado y guardado de puntuacion

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Puntuacion = require('./models/puntuacion')
 
 var app = express();
 
@@ -13,42 +15,62 @@ app.get('/', (req,res)=>{
 })
 
 app.get('/puntuaciones', (req,res)=>{
-    //TODO: leer la base de datos select
-    let datosJSON = {
-        accion:'get all',
-        datos: [
-            {nombre:'pepe', puntuacion:33},
-            {nombre:'bea', puntuacion:23},
-            {nombre:'felix', puntuacion:29}
-        ]
-    }
-    res.status(200).send(datosJSON);
+    Puntuacion.find().exec( (err, puntuaciones)=>{
+        if(err){
+            res.status(500).send({accion:'get all', mensaje:'error al obtener la puntuacion'})
+        }else{
+            res.status(200).send({accion:'get all', datos: puntuaciones})
+        }
+    })
+    
 })
 
 app.post('/puntuacion/', (req,res)=>{
     var datos = req.body;
     //TODO: Insertar en la base de datos insert
-    let datosJsonRespuesta={
-        accion:'save',
-        datos: datos
-    }
-    res.status(200).send(datosJsonRespuesta)
+    var puntuacion = new Puntuacion();
+    puntuacion.nombre = datos.nombre;
+    puntuacion.puntuacion = datos.puntuacion;
+    puntuacion.save( (err, puntuacionGuardada)=>{
+        if(err){
+            res.status(500).send({accion:'save', mensaje: 'Error al guardar la puntuacion'})
+        }else{
+            res.status(200).send({accion:'save', datos: puntuacionGuardada})
+        }
+    })
+
+    
 })
 
 
 
 app.delete('/puntuacion/:id',(req,res)=>{
     let puntuacionId = req.params.id;
-    let datosJsonRespuesta = {
-        accion: 'delete',
-        datos: puntuacionId
-    }
-    //TODO: borrar la base de datos el id
-    res.status(200).send(datosJsonRespuesta)
+    Puntuacion.findByIdAndDelete(puntuacionId, (err, puntuacionBorrada)=>{
+        if(err){
+            res.status(500).send({accion:'delete', mensaje: 'Error al borrar la puntuacion'})
+        }if(!puntuacionBorrada){
+            res.status(404).send({accion:'delete', mensaje: 'Error el id a borrar no existe'})
+        }
+        else{
+            res.status(200).send({accion:'delete', datos: puntuacionBorrada})
+        }
+    })
+    
 })
+
+//Nos conectamos a mongo dockerizado
+mongoose.connect('mongodb://localhost:27017/scores', (err, res)=>{
+    if(err){
+        console.log('Error al conectarme a la base de datos')
+        throw err
+    }else{
+        console.log('Conexión correcta a mongoDB')
+    }
+});
+
 
 
 app.listen(5200, ()=>{
     console.log("API REST funcionando en http://localhost:5200")
-})
-
+});
---
+++ models/puntuacion.js
@@ -0,0 +1,11 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema
+let PuntuacionSchema = Schema(
+    {
+        _id: {type: Schema.ObjectId, auto:true},
+        nombre: String,
+        puntuacion: Number
+    }
+)
+
+module.exports = mongoose.model('Score', PuntuacionSchema)
